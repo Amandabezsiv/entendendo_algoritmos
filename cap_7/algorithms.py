672182f6f5c8c48352746c4b41a78e107ce1b84d
Amandabezsiv <amandasousa211@gmail.com>
2024-03-20 20:24:56 -0300

    Bellman-Ford

--- cap_7/algorithms.py
+++ cap_7/algorithms.py
@@ -5,7 +5,7 @@ def dijkstra(graph, origin, destiny):
         graph Dict[List<Tuple>]: adjacency dict representation of a graph
         origin Int: vertex representing origin to dijkstra algorithm
         destiny Int: vertex representing destiny to dijkstra algorithm
-    complexity: O(V)
+    complexity: O(V^2)
     """
     best_path = [float("inf")] * len(graph)
     best_path[origin - 1] = 0
@@ -54,8 +54,41 @@ def floyd_warshal(graph):
                     distance_v[i][j] = distance_v[i][k] + distance_v[k][j]
                     distance_v[j][i] = distance_v[i][k] + distance_v[k][j]
 
-    return distance_v                          
-                
+    return distance_v
+
+def bellman_ford(graph, source):
+    """
+    Bellman-ford: shortest path between an source and all vertices, negative weight allowd
+    args:
+        graph Dict[List<Tuple>]: adjacency dict representation of a graph
+        sourcer Int: vertex representing origin
+    complexity: O(V*E)
+    """
+    
+    def update(u,v,l):
+    """
+    update: returns the shortest diastance
+    args:
+        u Int: distance of vertex u 
+        v Int: distance of vertex v
+        l Int: distance between u,v    
+    """
+        
+        return min(v, u + l)
+        
+
+    v_len = len(graph)
+    dist = [float('inf')]* v_len
+    dist[source-1] = 0
+
+    for i in range(v_len - 1): V - 1
+        for u in graph: 
+            for v, l in graph[u]:  
+                dist[v-1] = update(dist[u-1],dist[v-1],l)
+
+    return dist
+
+    
     
 
 g = {1: [(2, 1), (3, 5)], 2: [(4, 100)], 3: [(4, 5)], 4: []}
@@ -67,5 +100,8 @@ print(dijkstra(g,1,4), '\n')
 print('Floyd-warshal')      
 for i in floyd_warshal(g):
     print(i)
+
+print('Bellman-Ford source(1)')
+print(bellman_ford(g,1))
 
 
